1257c070b90fa146c74a310998e370ca833c7dbd
Darianom <darianosoares@hotmail.com>
2016-10-20 01:29:24 -0200

    Mover e remover arquivo.

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 var chokidar = require('chokidar');
 var handleCsv = require('./handleCsv');
+var fs = require("fs-extra");
 
 var config = require('./config/config')();
 require('./config/database.js')(config.db);
 
 var Model = require('./model')();
 
-console.log(Model);
-
 var watcher = chokidar.watch('./files/', {
   ignored: /[\/\\]\./, persistent: true
 });
 
 watcher
-  .on('add', path => {
-    handleCsv(path)
-      .on("data", function (data) {
-        console.log('Salva no banco de dados.');
-        Model
-          .create(data)
-          .then(res => console.log(res))
-          .catch(erro => console.log(erro));
-      })
-      .on("end", function () {
-        console.log("move arquivo para outro diretório.");
-      });
-  });
+  .on('add', escutaEvento);
+
+function escutaEvento(path) {
+  handleCsv(path)
+    .on("data", salva)
+    .on("end", removeArquivo(path));
+}
+
+function salva(dados) {
+  Model
+    .create(dados)
+    .then(res => console.log(res))
+    .catch(erro => console.log(erro));
+}
+
+function removeArquivo(path) {
+  return () => {
+    fs.move(path,'old-' + path, { clobber: true}, () => fs.remove(path));
+  };
+}
